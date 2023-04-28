3a29c1cfb12765e8de265c7fff5c69e0298e5cb4
MEHUL4951 <129840488+MEHUL4951@users.noreply.github.com>
2023-04-28 10:01:47 +0530

    Delete PAI-MEHUL/Mybook directory

--- PAI-MEHUL/Mybook/classes/autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-   session_start();
-   include("classes/config.php");
-   include("classes/login.php");
-   include("classes/user.php");
-   include("classes/post.php");
-   include("classes/image.php");
-   include("classes/profile.php");
